32354745265eb4b0854616a8b4b7422f330625cb
Sunattu <valentyn454@gmail.com>
2026-01-14 22:42:04 +0200

    Меню мапи + переключення в менюшках + Апте сосо

---
+++ Assets/scripts/game/Inventory/map/map_menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class map_menu : MonoBehaviour
+{
+    [SerializeField] private GameObject map;
+     private bool Map_menu_activated = false;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ��� ���������� ���������
+        if (Input.GetButtonDown("Map"))
+        {
+            Map_menu_activated = !Map_menu_activated;
+            map.SetActive(Map_menu_activated);
+
+        }
+    }
+}
+
--- Assets/scripts/game/Inventory_manager.cs
+++ Assets/scripts/game/Inventory_manager.cs
@@ -5,22 +5,87 @@ using UnityEngine;
 
 public class Inventorymenager : MonoBehaviour
 {
-    public GameObject inventory_menu;
+    public enum MenuType
+    {
+        None,
+        Inventory,
+        Map
+
+    }
+
+    [SerializeField] private GameObject inventory_menu;
+    [SerializeField] private GameObject map_menu;
+    [SerializeField] private GameObject main_menu;
+
     private bool Inventory_activated = false;
+    private bool map_activated = false;
+    
+
+    MenuType currectMenu = MenuType.None;
+
     void Start()
     {
 
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        // код відкривання Інвентарю
-        if (Input.GetButtonDown("Inventory"))
+        Openmenu_button();
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Closeall();
+
+        }
+    }
+    public void Toggle(MenuType menu)
+    {
+        if (currectMenu == menu)
+        {
+            Closeall();
+            currectMenu = MenuType.None;
+            return;
+        }
+
+        Closeall();
+        Activemainmenu();
+        Get(menu).SetActive(true);
+        currectMenu = menu;
+    }
+    void Closeall()
+    {
+        inventory_menu.SetActive(false);
+        map_menu.SetActive(false);
+        main_menu.SetActive(false);
+    }
+    GameObject Get(MenuType menu)
+    {
+        return menu switch
+        {
+            MenuType.Inventory => inventory_menu,
+            MenuType.Map => map_menu,
+            _ => null
+        };
+    }
+    void Openmenu_button()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.I))
         {
             Inventory_activated = !Inventory_activated;
             inventory_menu.SetActive(Inventory_activated);
-
+            Toggle(MenuType.Inventory);
         }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            map_activated =! map_activated;
+            map_menu.SetActive(map_activated);
+            Toggle(MenuType.Map);
+        }
+    }
+    void Activemainmenu()
+    {
+        main_menu.SetActive(true);
     }
 }
